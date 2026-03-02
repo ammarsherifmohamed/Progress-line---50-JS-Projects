04dac2fc780e89f0889a2427f0d77e0a6185194b
Ammar Sherif Mohamed <ammar.s.fouad555@gmail.com>
2026-03-02 14:15:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+let currentIndex = 1;
+
+next.addEventListener("click", () => {
+  currentIndex++;
+
+  if (currentIndex > circles.length) {
+    currentIndex = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentIndex--;
+
+  if (currentIndex < 1) {
+    currentIndex = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentIndex) {
+      circle.classList.add("active");
+
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  
+  progress.style.width = (actives.length -1) / (circles.length -1)
+  * 100 + "%"
+  
+  if(currentIndex === 1){
+      prev.disabled= true;
+  
+  }else if(currentIndex === circles.length){
+      next.disabled = true;
+  
+  }else{
+      prev.disabled = false;
+      next.disabled = false;
+  }
+  
+}
+
